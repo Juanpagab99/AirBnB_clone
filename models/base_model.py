016c27c6baa6c31abc585c19a838afed7ae2f7e8
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-06-29 16:57:55 +0000

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
+
+
 class BaseModel:
     """Class Basemodel that defines
     all methods for other classes"""
@@ -10,7 +12,6 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
 
     def __str__(self):
         """print the following format"""
