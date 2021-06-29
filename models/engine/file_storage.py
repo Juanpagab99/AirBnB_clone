cd34ec2bf269c419f060ef8308e0f97fe7b3b31c
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-06-29 22:00:11 +0000

    JSON

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,8 +25,13 @@ class FileStorage:
 	def save(self):
 		"""Serialize __objects
 		to JSON file path"""
+		dict1 = {}
+		for key, value in FileStorage.__objects.items():
+			dict1[key] = value.to_dict()
+			with open(FileStorage.__file_path, "w") as f:
+				json.dump(dict1, f)
 
 
-	def reload(self):
+	"""def reload(self):"""
 		"""deserializes the JSON
 		file to __objects"""
