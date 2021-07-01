f76eb194972c24eea450a618982c9b6dcdd28233
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-07-01 03:42:18 +0000

    more classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This file contains amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+	"""Public class attributes"""
+	name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This file contains state class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+	"""Public class attributes"""
+	name = ""
